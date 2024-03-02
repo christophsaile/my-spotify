cec4d4d681d3d9e55b4962bd08cc008173e30263
Christoph <christoph.saile@googlemail.com>
2024-03-02 22:08:53 +0100

    chore: fix broken build

--- components/currently-playing.tsx
+++ components/currently-playing.tsx
@@ -12,7 +12,7 @@ export default async function CurrentlyPlaying() {
 
   const isPlaying = song.is_playing;
   const title = song.item.name;
-  const artist = song.item.artists.map((_artist) => _artist.name).join(', ');
+  const artist = song.item.artists.map((_artist: any) => _artist.name).join(', ');
   const albumImageUrl = song.item.album.images[0].url;
   const songUrl = song.item.external_urls.spotify;
 

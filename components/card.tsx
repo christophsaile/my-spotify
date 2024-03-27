fcd95f59c5288659ce4b59fa3c770ca37e3caccd
Christoph <christoph.saile@googlemail.com>
2024-03-27 16:39:11 +0100

    feat: add animations

--- components/card.tsx
+++ components/card.tsx
@@ -9,15 +9,17 @@ export default function Card(props: {
   return (
     <a
       href={props.link}
-      className='h-full w-full flex gap-6 px-4 py-4 bg-neutral-800 hover:bg-neutral-700 rounded-xl focus:bg-neutral-700 focus:outline-4 focus:outline-dashed focus:outline-green-600'
+      className='group h-full w-full relative flex gap-6 px-4 py-4 bg-neutral-800 hover:bg-neutral-700 rounded-xl focus:bg-neutral-700 focus:outline-4 focus:outline-dashed focus:outline-green-600 transition-background'
     >
-      <Image
-        className='min-w-20 w-20 h-20 rounded-sm'
-        alt=''
-        src={props.image}
-        width={300}
-        height={300}
-      ></Image>
+      <div className='w-20 h-20 relative shrink-0 flex transform-three-d after:vinyl after:content-[""] left-[0] after:-z-10 after:rounded-full after:h-[94%] after:w-[94%] after:absolute after:left-[3%] after:top-[3%] before:content-[""] before:rounded-sm before:-z-10 before:bg-text-lvl-4 before:h-full before:w-full before:absolute before:right-[0px] before:top-[0] before:transition-all before:duration-[400ms] before:ease-in-out before:delay-[100ms] group-hover:album-rotate group-hover:-left-4 group-hover:after:left-10 transition-all duration-500 ease-in-out after:transition-all after:duration-[400ms] after:ease-in-out after:delay-[100ms] group-hover:before:-right-[4px]'>
+        <Image
+          className='min-w-20 w-20 h-20 rounded-sm'
+          alt=''
+          src={props.image}
+          width={300}
+          height={300}
+        ></Image>
+      </div>
       <div>
         <h3 className='mb-2 text-sm'>{props.title}</h3>
         <p className='text-neutral-400 text-xs'>{props.description}</p>
--- components/playlists.tsx
+++ components/playlists.tsx
@@ -25,18 +25,20 @@ export default async function Playlists() {
         {filteredPlaylists.map((playlist: any) => (
           <li key={playlist.id}>
             <a
-              className='flex flex-col gap-2 w-full rounded-lg focus:outline-4 focus:outline-dashed focus:outline-green-600'
+              className='group relative flex flex-col gap-2 w-full rounded-lg focus:outline-4 focus:outline-dashed focus:outline-green-600'
               href={playlist.external_urls.spotify}
               target='_blank'
               rel='noreferrer'
             >
-              <Image
-                className='aspect-square rounded-lg'
-                src={playlist.images[0].url}
-                alt=''
-                width={250}
-                height={250}
-              />
+              <div className='relative shrink-0 flex transform-three-d after:vinyl after:content-[""] left-[0] after:-z-10 after:rounded-full after:h-[94%] after:w-[94%] after:absolute after:left-[3%] after:top-[3%] before:content-[""] before:rounded-sm before:-z-10 before:bg-text-lvl-4 before:h-full before:w-full before:absolute before:right-[0px] before:top-[0] before:transition-all before:duration-[400ms] before:ease-in-out before:delay-[100ms] group-hover:album-rotate group-hover:-left-6 group-hover:after:left-12 transition-all duration-500 ease-in-out after:transition-all after:duration-[400ms] after:ease-in-out after:delay-[100ms] group-hover:before:-right-[4px]'>
+                <Image
+                  className='aspect-square rounded-lg'
+                  src={playlist.images[0].url}
+                  alt=''
+                  width={250}
+                  height={250}
+                />
+              </div>
               <span>{playlist.name}</span>
             </a>
           </li>
